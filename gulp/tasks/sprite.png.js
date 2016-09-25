3c7073c73c79595aa2af866706f336c32dfb3e94
Yuriy <uktrance@gmail.com>
2016-09-26 00:13:27 +0300

    26.09.2016 001

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -5,20 +5,18 @@ module.exports = function() {
     var spriteData = $.gulp.src('./source/images/png-for-sprite/*.png')
       .pipe($.gp.spritesmith({
         imgName: 'sprite.png',
-        cssName: 'sprite.css'
+        imgPath: '../img/sprite.png',
+        cssName: 'sprite.scss'
       }));
 
     var imgStream = spriteData.img
-      .pipe($.buffer())
-      .pipe($.gp.imagemin({
-        use: [$.optPNG()]
-      }))
+      //.pipe($.buffer())
+      //.pipe($.gp.imagemin({use: [$.optPNG()]}))
       //.pipe($.gp.tinypng('VI6Y8kDOlJPat45s63ccM4jM5a4Vw7QR'))
       .pipe($.gulp.dest($.config.root + '/assets/img'));
     
     var cssStream = spriteData.css
-      .pipe($.gp.csso())
-      .pipe($.gulp.dest($.config.root + '/assets/css'));
+      .pipe($.gulp.dest('./source/style/common'));
 
     return $.merge(imgStream, cssStream);
   })
