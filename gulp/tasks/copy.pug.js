b5d40eb033ec4eef7c87255ea8c7fb2ea50d52e4
Yuriy <uktrance@gmail.com>
2016-10-21 06:22:43 +0300

    export pug

--- gulp/tasks/copy.pug.js
+++ gulp/tasks/copy.pug.js
@@ -3,11 +3,9 @@
 module.exports = function() {
   $.gulp.task('copy:pug', function() {
     return $.gulp.src('./source/**/*.pug', { since: $.gulp.lastRun('copy:pug') })
-    	//.pipe($.gp.imagemin())
-    	//.pipe($.gp.flatten())
-    	.pipe($.gp.rename({
-			extname: '.jade'
-		}))
+  		// .pipe($.gp.rename({
+		// 	extname: '.jade'
+		// }))
       	.pipe($.gulp.dest($.config.pugImportPath));
   	});
 };
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,6 +5,7 @@ module.exports = function() {
     $.gulp.watch('./source/blocks/**/*.js', $.gulp.series('js:process'));
     $.gulp.watch('./source/**/*.scss', $.gulp.series('sass'));
     $.gulp.watch('./source/**/*.pug', $.gulp.series('pug'));
+    $.gulp.watch('./source/**/*.pug', $.gulp.series('copy:pug'));
     $.gulp.watch(['./source/images/**/*.+(jpg|jpeg|png|gif)', './source/static/images/*.+(jpg|jpeg|png|gif)'], $.gulp.series('copy:image'));
     $.gulp.watch('./source/blocks/**/sprite/*.svg', $.gulp.series('sprite:svg'));
     $.gulp.watch('./source/blocks/**/sprite/*.png', $.gulp.series('sprite:png'));
